9a9feafbf16600261720c2e2ef487271ef0733c4
Tabroid <pengc2019@gmail.com>
2022-10-21 15:09:10 -0500

    fix the size regulator

--- tools/data_reformat.py
+++ tools/data_reformat.py
@@ -14,8 +14,15 @@ DATA_DIR = os.path.join(ROOT_DIR, 'data')
 RAWDATA_PATH = os.path.join(DATA_DIR, 'MD_Inv.xlsx')
 CAT_SIZE_PATH = os.path.join(ROOT_DIR, 'search', 'static', 'cat_size.json')
 
+# datetime tags
+# DATE = datetime.date.today().strftime('%Y-%m-%d')
+# DATETIME = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+DATE = '2022-10-20'
+DATETIME = '2022-10-20 00:00:00'
 
-def regulated_size(cat_size_book, vcat, vsize):
+
+def regulated_size(cat_size_book, vcat, vsize, verbose=0, null_value='NULL'):
+    # print(vcat, vsize)
     # check category
     if vcat not in cat_size_book.keys():
         for key in cat_size_book.keys():
@@ -23,8 +30,9 @@ def regulated_size(cat_size_book, vcat, vsize):
                 vcat = key
                 break
     if vcat not in cat_size_book.keys():
-        print('Category {} is not found in the book!'.format(vcat))
-        return 'NULL'
+        if verbose > 0:
+            print('Category {} is not found in the book!'.format(vcat))
+        return null_value
 
     sizes = [s for s in cat_size_book[vcat]]
     vsize = str(vsize)
@@ -32,11 +40,15 @@ def regulated_size(cat_size_book, vcat, vsize):
         return vsize
 
     for s in sizes:
-        if SequenceMatcher(None, s, vsize).ratio() > 0.5:
-            print('Size {} has no exact match in cateogry {}, but we found a similar one {}.'.format(vsize, vcat, s))
+        ratio = SequenceMatcher(None, s, vsize).ratio()
+        if ratio >= 0.5:
+            if verbose > 0:
+                print('Size {} has no exact match in cateogry {}, '
+                      'but we found a similar one {} with matching ratio {:.2f}.'.format(vsize, vcat, s, ratio))
             return s
-    print('Size {} has no exact match in cateogry {}, and no similar one found!'.format(vsize, vcat))
-    return 'NULL'
+    if verbose > 0:
+        print('Size {} has no exact match in cateogry {}, and no similar one found!'.format(vsize, vcat))
+    return null_value
 
 
 if __name__ == '__main__':
@@ -44,7 +56,7 @@ if __name__ == '__main__':
     cat_size = json.load(open(CAT_SIZE_PATH))
 
     # read data file and regulate header names
-    df = pd.read_excel(RAWDATA_PATH, header=0).rename(columns={'Catogary1': 'Brand', 'Catogary2': 'Category'})
+    df = pd.read_excel(RAWDATA_PATH, header=0).rename(columns={'Category1': 'Brand', 'Category2': 'Category'})
     df.columns = [x.lower().replace(' ', '_') for x in df.columns]
     # header checks
     required = ['brand', 'category', 'name', 'sku', 'size', 'stock_x', 'count']
@@ -58,7 +70,12 @@ if __name__ == '__main__':
 
     # regulate the category/size
     df['size'] = df['size'].astype(str).str.upper()
-    df.loc[:, 'rsize'] = df.apply(lambda x: regulated_size(cat_size, x.category, x.size), axis=1)
+    df.loc[:, 'rsize'] = df.apply(lambda x: regulated_size(cat_size, x['category'], x['size']), axis=1)
+    mask = df['rsize'] == 'NULL'
+    if sum(mask):
+        print('WARNING')
+        print('{} entries cannot find correct size in the size book.'.format(sum(mask)))
+        print(df.loc[df['rsize'] == 'NULL'])
 
     # get product table
     dfp = df.groupby('sku').agg({'name': 'first', 'brand': 'first', 'category': 'first'})\
@@ -66,24 +83,22 @@ if __name__ == '__main__':
     dfp.loc[:, 'id'] = ''
     dfp.loc[:, 'image_src'] = ''
     dfp.columns = [x.lower() for x in dfp.columns]
-    dfp.to_csv(os.path.join(DATA_DIR, 'Product-{}.csv'.format(datetime.date.today().strftime('%Y-%m-%d'))), index=False)
+    dfp.to_csv(os.path.join(DATA_DIR, 'Product-{}.csv'.format(DATE)), index=False)
 
     # get product item table
     item_data = []
     begin = 1
     stockx_price_inc = 100
-    today = datetime.date.today().strftime('%Y-%m-%d')
-    now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     for sku, size, stockx_price, count in df[['sku', 'rsize', 'stock_x', 'count']].values:
         ref_price = np.max([float(x) for x in str(stockx_price).split('/')])
         price = ref_price + stockx_price_inc
         for i in np.arange(begin, count+begin):
-            item_id = '{}-{:06d}'.format(today, i)
+            item_id = '{}-{:06d}'.format(DATE, i)
             item_data.append([
-                    item_id, sku, price, stockx_price, size, now, 'N/A', 'Kickz Maryland',
+                    item_id, sku, price, stockx_price, size, DATETIME, 'N/A', 'Kickz Maryland',
             ])
         begin += count
     dfi = pd.DataFrame(columns=['id', 'product', 'price', 'stockx_price', 'size', 'added_on', 'storage_loc', 'storage_addr'],
                        data=item_data)
     dfi['price'] = dfi['price'].fillna(1500)
-    dfi.to_csv(os.path.join(DATA_DIR, 'Product-Item-{}.csv'.format(datetime.date.today().strftime('%Y-%m-%d'))), index=False)
+    dfi.to_csv(os.path.join(DATA_DIR, 'Product-Item-{}.csv'.format(DATE)), index=False)
