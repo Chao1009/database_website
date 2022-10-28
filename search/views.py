19189c1cda26ea96088ea128d2e0c6e9f11b1ba1
Tabroid <pengc2019@gmail.com>
2022-10-27 21:17:42 -0500

    add custom page behavior, auto correct out-of-range page numbers

--- search/views.py
+++ search/views.py
@@ -11,6 +11,8 @@ from django.conf import settings
 from django.shortcuts import render, redirect
 from django.db.models import Q
 from django.views.generic import ListView, DetailView, View
+from django.core.paginator import InvalidPage
+from django.http import Http404
 
 from .models import *
 from django.contrib.staticfiles.storage import staticfiles_storage
@@ -63,6 +65,28 @@ class HomeView(ListView):
             brands.sort(key=lambda x: len(x['models']), reverse=True)
     except OperationalError:
         print('Warning: no product data entries')
+
+    def paginate_queryset(self, queryset, page_size):
+        """Paginate the queryset, if needed."""
+        paginator = self.get_paginator(
+        queryset, page_size, orphans=self.get_paginate_orphans(),
+                allow_empty_first_page=self.get_allow_empty())
+        page_kwarg = self.page_kwarg
+        page = self.kwargs.get(page_kwarg) or self.request.GET.get(page_kwarg) or 1
+        try:
+            page_number = int(page)
+        except ValueError:
+            if page == 'last':
+                page_number = paginator.num_pages
+            else:
+                raise Http404(_("Page is not 'last', nor can it be converted to an int."))
+        try:
+            page = paginator.page(page_number)
+            return (paginator, page, page.object_list, page.has_other_pages())
+        except InvalidPage as e:
+            page_number = paginator.num_pages #<-last page
+            page = paginator.page(page_number)
+            return (paginator, page, page.object_list, page.has_other_pages()) #<-return last page
 
     def get_queryset(self):
         print(self.request.GET)
